e0c1bce35e2351e81f0b9bd58584ed8f7e38533d
Helouj <JacobHelouZX@gmail.com>
2019-06-26 14:58:16 -0400

    added query to store and retrieve notes from a specific user

--- ElevenNote.Data/Note.cs
+++ ElevenNote.Data/Note.cs
@@ -13,7 +13,7 @@ namespace ElevenNote.Data
 
         public int NoteID { get; set; }
         [Required]
-        public Guid OwnerID { get; set; }
+        public Guid OwnerId { get; set; }
         [Required]
         public string Title { get; set; }
         [Required]
